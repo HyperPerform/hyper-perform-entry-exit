3671fbba634c74719a972e0b43ae87fa50de03d0
RohanChhipa <rohan.chhipa@live.com>
2016-10-04 12:15:55 +0200

    added honeywell reader code

--- honeywell/src/main/java/me/hyperperform/entryexit/Main.java
+++ honeywell/src/main/java/me/hyperperform/entryexit/Main.java
@@ -1,12 +1,80 @@
 package me.hyperperform.entryexit;
 
-/**
- * Created by Rohan on 2016-10-04.
- */
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Main
 {
+    public Main()
+    {
+        super();
+    }
+    
+    void connect ( String portName ) throws Exception
+    {
+    	System.out.println("Connecting...");
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if ( portIdentifier.isCurrentlyOwned() )
+        {
+            System.out.println("Error: Port is currently in use");
+        }
+        else
+        {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+            
+            System.out.println("Checking if serial port instance...");
+            if ( commPort instanceof SerialPort )
+            {
+            	System.out.println("Is serial port...");
+
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(57600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+                
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+                
+
+                //---------------------------------------------------------------------------------------------
+                byte[] buffer = new byte[1024];
+	            int len = -1;
+	            try
+	            {	
+	            	System.out.println("Started reading from device...");
+	                while ((len = in.read(buffer)) > -1)
+	                	if (len > 0)
+	                    	System.out.println("---" + new String(buffer) + "---");
+	            }
+	            catch ( IOException e )
+	            {
+	                e.printStackTrace();
+	           		System.out.println("Ended reading by exception...");
+	            }            
+                //---------------------------------------------------------------------------------------------
+
+
+            }
+            else
+            {
+                System.out.println("Error: Only serial ports are handled by this Main.");
+            }
+        }     
+    }
+    
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        try
+        {
+            (new Main()).connect("COM3");
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
     }
 }
